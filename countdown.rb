ed005583fc8e4c324f8885e5295d0e78cd382e67
Harry Bloch <harryfbloch@gmail.com>
2018-09-18 19:26:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,12 @@ def countdown(seconds)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep 
+def countdown_with_sleep(seconds)
+  seconds += 5
+  while seconds > 0 
+   puts "#{seconds} SECOND(S)!"
+   seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
   
 end
